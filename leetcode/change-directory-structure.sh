cf1e680bb27e1ac65ae0907a0c7922e6c672248d
Lihao WANG <impatrickwang@hotmail.com>
2017-06-04 00:25:08 +0800

    Revert "[Lihao WANG] use bash to change the directory structure" then do it again
    This reverts commit 08b07077b42b15bc51555abb7d21193e93a67342.

--- leetcode/change-directory-structure.sh
+++ leetcode/change-directory-structure.sh
@@ -6,7 +6,7 @@ index=1
 for line in `cat ${LIST}`
 do
 	mkdir ${index}
-	mv ${line}.java ${index}
-	echo "move ${line}.java into ${index}"
+	mv ${line}.java ${index}/Solution.java
+	echo "move ${line}.java into ${index}/Solution.java"
 	((index=index+1))
 done
