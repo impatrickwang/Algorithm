8a4e08c9c6eeb683913d0041de37c465dfef6510
Lihao WANG <impatrickwang@hotmail.com>
2017-06-09 22:20:34 +0800

    [Lihao WANG] leetcode problems 63. Unique Paths II

---
+++ leetcode/Algorithms/63/Solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+	// 22:15
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        if (m < 1) return 0;
+        int n = obstacleGrid[0].length;
+        if (n < 1) return 0;
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) dp[i][j] = 0;
+                else {
+                    if (i == 0 && j == 0) dp[i][j] = 1;
+                    else if (i == 0) dp[i][j] = dp[i][j-1];
+                    else if (j == 0) dp[i][j] = dp[i-1][j];
+                    else dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
