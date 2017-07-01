3ba904f26980f6387c84bc96772472a5e8fd1dbd
Lihao WANG <impatrickwang@hotmail.com>
2017-07-01 22:18:27 +0800

    [Lihao WANG] leetcode problems 100. Same Tree

---
+++ leetcode/Algorithms/100/Solution.java
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+	// 22:11
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (isSameTree(p.left, q.left) && isSameTree(p.right, q.right) && p.val == q.val) return true;
+        return false;
+    }
+}
