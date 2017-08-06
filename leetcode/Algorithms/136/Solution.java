1f1177e64b9ec860f85697c456a5e06125e09948
Lihao WANG <impatrickwang@hotmail.com>
2017-08-06 17:37:56 +0800

    [Lihao WANG] leetcode problems 136. Single Number

---
+++ leetcode/Algorithms/136/Solution.java
@@ -0,0 +1,8 @@
+public class Solution {
+	// 17:35
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) res ^= nums[i];
+        return res;
+    }
+}
