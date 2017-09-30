066a8735c206c149affa1578e00bea743bbed0f7
Lihao WANG <impatrickwang@hotmail.com>
2017-09-30 13:17:26 +0800

    [Lihao WANG] leetcode problems 235. Lowest Common Ancestor of a Binary Search Tree

---
+++ leetcode/Algorithms/235/Solution.java
@@ -0,0 +1,16 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root.val == p.val || root.val == q.val || (root.val - q.val) * (root.val - p.val) < 0) return root;
+        if (root.val > p.val) return lowestCommonAncestor(root.left, p, q);
+        return lowestCommonAncestor(root.right, p, q);
+    }
+}
