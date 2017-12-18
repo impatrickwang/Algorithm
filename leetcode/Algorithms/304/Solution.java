8fc8b80fecb15bee166c145e85c62e96a73f8e06
Lihao WANG <impatrickwang@hotmail.com>
2017-12-18 22:39:00 +0800

    [Lihao WANG] leetcode problems 304. Range Sum Query 2D - Immutable

---
+++ leetcode/Algorithms/304/Solution.java
@@ -0,0 +1,32 @@
+class NumMatrix {
+
+    private int[][] sums;
+
+    public NumMatrix(int[][] matrix) {
+        int m = matrix.length;
+        if (m > 0) {
+            int n = matrix[0].length;
+            if (n > 0) {
+                sums = new int[m+1][n+1];
+                for (int i = 0; i < m; i++) {
+                    for (int j = 0; j < n; j++) {
+                        if (i == 0 && j == 0) sums[i+1][j+1] = matrix[i][j];
+                        else if (i == 0) sums[i+1][j+1] = sums[i+1][j]+matrix[i][j];
+                        else if (j == 0) sums[i+1][j+1] = sums[i][j+1]+matrix[i][j];
+                        else sums[i+1][j+1] = sums[i+1][j]+sums[i][j+1]-sums[i][j]+matrix[i][j];
+                    }
+                }
+            }
+        }
+    }
+    
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return sums[row2+1][col2+1] - sums[row1][col2+1] - sums[row2+1][col1] + sums[row1][col1];
+    }
+}
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix obj = new NumMatrix(matrix);
+ * int param_1 = obj.sumRegion(row1,col1,row2,col2);
+ */
